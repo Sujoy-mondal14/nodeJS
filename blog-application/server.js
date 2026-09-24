8a21d27a6db8c400c266c3517b196392f5d2c861
Sujoy Mondal <hackathontest07@gmail.com>
2026-09-24 20:33:48 +0530

    blog applicatin complete

--- blog-application/server.js
+++ blog-application/server.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const app = require('./app.js')
 const path = require('path')
 const {ConnectDB} = require('./dbConnection.js')
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 //Data Base
-ConnectDB("mongodb://127.0.0.1:27017/blog-application")
+// ConnectDB("mongodb://127.0.0.1:27017/blog-application")
+ConnectDB(process.env.MONGO_URL)
 
 // view engine
 app.set('view engine', 'ejs')
